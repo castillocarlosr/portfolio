f78426aa7cb2beff43cd2c3d4e4de8700be369ee
castillocarlosr <castillocarlosr@gmail.com>
2018-11-28 12:57:48 -0800

    changed error message on server

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', (request, response) => {
   response.sendFile('index.html', {root: './public'});
 });
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.  I am currently working on this hope to get this up and running.  Last update worked on Nov. 28, 2018.'));
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
